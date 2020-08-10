270aab030b5030aba28a0cf023834cadd38665fd
sandraols <sandra.helene.olsson@gmail.com>
2020-08-10 10:59:10 +0200

    connect logged in page

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ const loginBtn = document.querySelector("#login-btn");
 const name = document.querySelector("#name");
 const pin = document.querySelector("#pin");
 const modal = document.querySelector("#modal");
+const loginPage = document.querySelector("#login-page");
+const loggedInPage = document.querySelector("#logged-in-page");
 
 loginBtn.addEventListener("click", function(event) {
   event.preventDefault();
@@ -41,9 +43,9 @@ const loginUser = (name, pin) => {
   if (user) {
     if (user.isBlocked) {
       modal.innerHTML += `<div class="modal-content">
-            <p class="modal-paragraph">User blocked.<br> No login attempts available.</p>
-            <button onclick="closeModal()" class="modal-btn">Ok</button>
-          </div>`;
+        <p class="modal-paragraph">User blocked.<br> No login attempts available.</p>
+        <button onclick="closeModal()" class="modal-btn">Ok</button>
+      </div>`;
       loginBtn.disabled = true;
       return;
     }
@@ -51,52 +53,54 @@ const loginUser = (name, pin) => {
       user.failedAttempts += 1;
       if (user.failedAttempts >= 3) {
         modal.innerHTML += `<div class="modal-content">
-            <p class="modal-paragraph">Login failed.<br> No more login attempts available.</p>
-            <button onclick="closeModal()" class="modal-btn">Ok</button>
-          </div>`;
+          <p class="modal-paragraph">Login failed.<br> No more login attempts available.</p>
+          <button onclick="closeModal()" class="modal-btn">Ok</button>
+        </div>`;
         user.isBlocked = true;
         loginBtn.disabled = true;
       } else {
         modal.innerHTML += `<div class="modal-content">
-            <p class="modal-paragraph">Login failed.<br> you have 
-            ${3 - user.failedAttempts} login attempts left.</p>
-            <button onclick="removeModalAndPin()" class="modal-btn">Ok</button>
-          </div>`;
+          <p class="modal-paragraph">Login failed.<br> you have 
+          ${3 - user.failedAttempts} login attempts left.</p>
+          <button onclick="removeModalAndPin()" class="modal-btn">Ok</button>
+        </div>`;
       }
     } else {
       console.log("logged in");
+      loginPage.classList.add('disabled');
+      loggedInPage.classList.remove('disabled');
       loggedInUser = user;
     }
   } else {
-    alert("User not found");
+    modal.innerHTML += `<div class="modal-content">
+      <p class="modal-paragraph">User not found</p>
+      <button onclick="removeModalAndPin()" class="modal-btn">Ok</button>
+    </div>`;
   }
 };
 
+
+const clearInputFields = () => {
+  name.value = "";
+  pin.value = "";
+};
+  const enableLoginBtn = () => {
+    loginBtn.disabled = false;
+};
 const closeModal = () => {
   modal.innerHTML = "";
   clearInputFields();
   enableLoginBtn();
 };
-const clearInputFields = () => {
-  name.value = "";
-  pin.value = "";
-};
-const enableLoginBtn = () => {
-  loginBtn.disabled = false;
-};
 const removeModalAndPin = () => {
     modal.innerHTML = "";
     pin.value = "";
 }
-// name.addEventListener('click', function() {
-//     clearInputFields();
-//     enableLoginBtn();
-// });
 
 // When user is logged in
 
 let loggedInUser;
-balanceBtn = document.getElementById("btn");
+balanceBtn = document.querySelector("#btn");
 balanceBtn.addEventListener("click", function() {
   checkBalance();
 });
